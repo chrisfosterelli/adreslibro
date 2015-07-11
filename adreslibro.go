2d82c4612f13083d6a6f777fc6029617331ad7b1
Chris Foster <chris.james.foster@gmail.com>
2015-07-10 21:09:25 -0700

    Replace GAE with basic golang server

--- adreslibro.go
+++ adreslibro.go
@@ -1,21 +1,58 @@
-package adreslibro
+package main
 
 import (
-    "html/template"
-    "net/http"
+	"html/template"
+    "gopkg.in/mgo.v2"
+	"net/http"
+    "log"
 )
 
-func init() {
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/esperantuloj/chrisfosterelli", handler2)
+type User struct {
+	Name     string
+	Email    string
+	Password string
+	Location string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    tem := template.Must(template.ParseFiles("template/index.html"))
-    tem.Execute(w, r.FormValue("Content"))
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/registri", register)
+	http.HandleFunc("/esperantuloj/chrisfosterelli", user)
+    log.Println("Starting Server")
+	http.ListenAndServe(":8080", nil)
 }
 
-func handler2(w http.ResponseWriter, r *http.Request) {
-    tem := template.Must(template.ParseFiles("template/user.html"))
-    tem.Execute(w, r.FormValue("Content"))
+func index(w http.ResponseWriter, r *http.Request) {
+	tem := template.Must(template.ParseFiles("template/index.html"))
+	tem.Execute(w, r.FormValue("Content"))
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		session, err := mgo.Dial("mongodb://adreslibro:adreslibro@dogen.mongohq.com:10096/adreslibro_dev")
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		u := User{
+			Name:     r.FormValue("name"),
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+			Location: r.FormValue("location"),
+		}
+		c := session.DB("").C("users")
+		err = c.Insert(&u)
+		if err != nil {
+			log.Fatal(err)
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		tem := template.Must(template.ParseFiles("template/register.html"))
+		tem.Execute(w, r.FormValue("Content"))
+	}
+}
+
+func user(w http.ResponseWriter, r *http.Request) {
+	tem := template.Must(template.ParseFiles("template/user.html"))
+	tem.Execute(w, r.FormValue("Content"))
 }
