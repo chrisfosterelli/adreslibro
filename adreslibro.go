1dc116c28f6a027296f85042ce88371107c80958
Chris Foster <chris.james.foster@gmail.com>
2015-07-14 21:53:01 -0700

    Remember the user after they login and display their session

--- adreslibro.go
+++ adreslibro.go
@@ -1,17 +1,48 @@
 package main
 
 import (
+    "log"
+	"net/http"
 	"html/template"
     "gopkg.in/mgo.v2"
-	"net/http"
-    "log"
+    "gopkg.in/mgo.v2/bson"
+    "github.com/gorilla/sessions"
 )
+
+var store = sessions.NewCookieStore([]byte("!adr3sl1br0s3cr3t5@"))
+var tmplt = template.Must(template.ParseGlob("template/*"))
 
 type User struct {
 	Name     string
 	Email    string
 	Password string
 	Location string
+}
+
+func getCurrentUser(r *http.Request) *User {
+    session, err := mgo.Dial("mongodb://adreslibro:adreslibro@dogen.mongohq.com:10096/adreslibro_dev")
+    if err != nil {
+        panic(err)
+    }
+    defer session.Close()
+    sessionz, _ := store.Get(r, "adreslibro")
+    name := sessionz.Values["user"]
+    c := session.DB("").C("users")
+    user := User{}
+    err = c.Find(bson.M{"name":name}).One(&user)
+    if err != nil {
+        return &user
+    }
+    return &user
+}
+
+func renderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data map[string]interface{}) {
+    data["currentUser"] = getCurrentUser(r)
+    log.Println(data["currentUser"])
+    err := tmplt.ExecuteTemplate(w, tmpl, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func main() {
@@ -23,8 +54,7 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	tem := template.Must(template.ParseFiles("template/index.html"))
-	tem.Execute(w, r.FormValue("Content"))
+    renderTemplate(w, r, "index", make(map[string]interface{}))
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
@@ -45,14 +75,15 @@ func register(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 		}
+        sessionz, _ := store.Get(r, "adreslibro")
+        sessionz.Values["user"] = u.Name
+        sessionz.Save(r, w)
 		http.Redirect(w, r, "/", http.StatusFound)
 	} else {
-		tem := template.Must(template.ParseFiles("template/register.html"))
-		tem.Execute(w, r.FormValue("Content"))
+        renderTemplate(w, r, "register", make(map[string]interface{}))
 	}
 }
 
 func user(w http.ResponseWriter, r *http.Request) {
-	tem := template.Must(template.ParseFiles("template/user.html"))
-	tem.Execute(w, r.FormValue("Content"))
+    renderTemplate(w, r, "user", make(map[string]interface{}))
 }
