67d3cfaa177487a6df2bac43f70c93686580c151
Chris Foster <chris.james.foster@gmail.com>
2015-07-30 20:00:33 -0700

    Fix login to use Mongo IDs

--- adreslibro.go
+++ adreslibro.go
@@ -13,23 +13,41 @@ var store = sessions.NewCookieStore([]byte("!adr3sl1br0s3cr3t5@"))
 var tmplt = template.Must(template.ParseGlob("template/*"))
 
 type User struct {
+	Id       bson.ObjectId `bson:"_id"`
 	Name     string
 	Email    string
 	Password string
 	Location string
 }
 
+//func getUser(email string) {
+    //user := User{}
+    //query := bson.M{"email":email}
+    //err := c.Find(query).One(&user)
+    //if err != nil {
+        //log.Error("Could not fetch user", err)
+    //}
+    //return &user
+//}
+
 func getCurrentUser(r *http.Request) *User {
-    session, err := mgo.Dial("mongodb://adreslibro:adreslibro@dogen.mongohq.com:10096/adreslibro_dev")
+    mgoSession, err := mgo.Dial("mongodb://adreslibro:adreslibro@dogen.mongohq.com:10096/adreslibro_dev")
     if err != nil {
         panic(err)
     }
-    defer session.Close()
-    sessionz, _ := store.Get(r, "adreslibro")
-    name := sessionz.Values["user"]
-    c := session.DB("").C("users")
+    defer mgoSession.Close()
+    userSession, _ := store.Get(r, "adreslibro")
+    stringId := userSession.Values["id"].(string)
+    log.Println(stringId)
     user := User{}
-    err = c.Find(bson.M{"name":name}).One(&user)
+    if !bson.IsObjectIdHex(stringId) {
+        log.Println("Not a valid Object ID Hex")
+        return &user
+    }
+    mgoId := bson.ObjectIdHex(stringId)
+    c := mgoSession.DB("").C("users")
+    err = c.FindId(mgoId).One(&user)
+    log.Println(err)
     if err != nil {
         return &user
     }
@@ -83,7 +101,7 @@ func login(w http.ResponseWriter, r *http.Request) {
             return
         }
         sessionz, _ := store.Get(r, "adreslibro")
-        sessionz.Values["user"] = user.Name
+        sessionz.Values["id"] = user.Id.Hex()
         sessionz.Save(r, w)
 		http.Redirect(w, r, "/", http.StatusFound)
     } else {
@@ -93,7 +111,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 func logout(w http.ResponseWriter, r *http.Request) {
     sessionz, _ := store.Get(r, "adreslibro")
-    sessionz.Values["user"] = ""
+    sessionz.Values["id"] = ""
     sessionz.Save(r, w)
     http.Redirect(w, r, "/", http.StatusFound)
 }
@@ -111,13 +129,16 @@ func register(w http.ResponseWriter, r *http.Request) {
 			Password: r.FormValue("password"),
 			Location: r.FormValue("location"),
 		}
+        u.Id = bson.NewObjectId()
 		c := session.DB("").C("users")
 		err = c.Insert(&u)
 		if err != nil {
 			log.Fatal(err)
 		}
+        log.Println("Result")
+        log.Println(u.Id.Hex())
         sessionz, _ := store.Get(r, "adreslibro")
-        sessionz.Values["user"] = u.Name
+        sessionz.Values["id"] = u.Id.Hex()
         sessionz.Save(r, w)
 		http.Redirect(w, r, "/", http.StatusFound)
 	} else {
